dd172763a3deedfa8d2608c3e45e6ef8941226f7
BrunoKappi <brunokappidematos2@gmail.com>
2023-03-30 23:28:46 -0300

    Comunnication with Firebase

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Profile.css'
 import { connect } from 'react-redux'
 import UserModal from '../UsersList/User/UserModal'
@@ -9,19 +9,22 @@ import { useNavigate } from 'react-router-dom';
 const Profile = (props) => {
 
     const navigate = useNavigate();
-    const [CurrentUser,] = useState({ ...props.Usuarios.find(user => user.Email === props.LoggedUser.Email) })
+    const [CurrentUser, SetCurrentUser] = useState({ ...props.Usuarios.find(user => user.Email === props.LoggedUser.Email) })
     const [modalShow, setModalShow] = useState(true);
 
 
+    useEffect(() => {
+        SetCurrentUser({ ...props.Usuarios.find(user => user.Email === props.LoggedUser.Email) })
+    }, [props.Usuarios])
 
     return (
         <div className={localStorage.getItem('AssetSenseTema') === 'Escuro' ? 'ProfileContainerEscuro ProfileContainer' : 'ProfileContainerClaro ProfileContainer'}>
 
-            <UserModal FromModal={false} CurrentUser={CurrentUser} User={CurrentUser} show={modalShow} onHide={() => {  setModalShow(false); navigate('../' + props.LoggedUser.CurrentSidebarTab);}} Function="View" />
+            <UserModal FromModal={false} CurrentUser={CurrentUser} User={CurrentUser} show={modalShow} onHide={() => { setModalShow(false); navigate('../' + props.LoggedUser.CurrentSidebarTab); }} Function="View" />
 
         </div>
     )
-} 
+}
 
 
 const ConnectedProfile = connect((state) => {
